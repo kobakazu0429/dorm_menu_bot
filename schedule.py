106f97fcfd9229cd77fecd82ca95636fea264367
kobakakzu0429 <kobakazu10@gmail.com>
2018-09-29 11:29:04 +0900

    in func

--- schedule.py
+++ schedule.py
@@ -3,25 +3,27 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from twitter_bot import postTwitter
 from line_bot import postLINE
 
-sched = BlockingScheduler()
+
+def schedule():
+    sched = BlockingScheduler()
+
+    @sched.scheduled_job('cron', hour='7')
+    def morning():
+        postTwitter()
+        postLINE()
+
+    @sched.scheduled_job('cron', hour='11', minute='30')
+    def lunch():
+        postTwitter()
+        postLINE()
+
+    @sched.scheduled_job('cron', hour='17')
+    def dinner():
+        postTwitter()
+        postLINE()
+
+    sched.start()
 
 
-@sched.scheduled_job('cron', hour='7')
-def morning():
-    postTwitter()
-    postLINE()
-
-
-@sched.scheduled_job('cron', hour='11', minute='30')
-def lunch():
-    postTwitter()
-    postLINE()
-
-
-@sched.scheduled_job('cron', hour='17')
-def dinner():
-    postTwitter()
-    postLINE()
-
-
-sched.start()
+if __name__ == '__main__':
+    schedule()
