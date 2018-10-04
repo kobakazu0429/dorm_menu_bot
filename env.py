f7fc9625ec58e06c539ba186d22dd3d74935964b
kobakakzu0429 <kobakazu10@gmail.com>
2018-10-04 11:25:06 +0900

    remove line-bot

--- env.py
+++ env.py
@@ -9,9 +9,3 @@ CK = os.environ.get('CK')
 CS = os.environ.get('CS')
 AK = os.environ.get('AK')
 AS = os.environ.get('AS')
-
-CHANNEL_ACCESS_TOKEN = os.environ.get('CHANNEL_ACCESS_TOKEN')
-CHANNEL_SECRET = os.environ.get('CHANNEL_SECRET')
-
-GAS_URL = os.environ.get('GAS_URL')
-GAS_TOKEN = os.environ.get('GAS_TOKEN')
--- schedule.py
+++ schedule.py
@@ -1,7 +1,6 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 from twitter_bot import postTwitter
-from line_bot import postLINE
 
 
 def schedule():
@@ -10,17 +9,14 @@ def schedule():
     @sched.scheduled_job('cron', hour='7')
     def morning():
         postTwitter()
-        postLINE()
 
     @sched.scheduled_job('cron', hour='11', minute='30')
     def lunch():
         postTwitter()
-        postLINE()
 
     @sched.scheduled_job('cron', hour='17')
     def dinner():
         postTwitter()
-        postLINE()
 
     sched.start()
 
